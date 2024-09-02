83be1f9ca92d6be4e2c76ee7765184da9f09ba97
domsdoms <116958892+domingodominic@users.noreply.github.com>
2024-09-02 13:43:02 +0800

    #finalizing the booking form

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import BookingForm from "./components/booking components/BookingForm";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("swiper/react", () => ({
+  Swiper: () => <div>Mocked Swiper</div>,
+  SwiperSlide: () => <div>Mocked Swiper Slide</div>,
+}));
+
+test("Renders the BookingForm heading", () => {
+  render(<BookingForm />);
+  w;
+  const headingElement = screen.getByText("Book Now");
+  expect(headingElement).toBeInTheDocument();
 });
